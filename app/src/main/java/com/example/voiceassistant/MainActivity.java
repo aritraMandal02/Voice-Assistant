adf2672c5a5fdc7275730c581f1d4880b3d1bb78
Aritra Mandal <aritra.mandal2002@gmail.com>
2021-12-15 20:34:42 +0530

    Updating the Layout

--- app/src/main/java/com/example/voiceassistant/MainActivity.java
+++ app/src/main/java/com/example/voiceassistant/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
